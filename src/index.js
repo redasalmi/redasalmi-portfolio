367acf19bbba1fb8f35c5ae77e6dd189595f8eac
redasalmi <reda.salmi.elt@gmail.com>
2020-01-24 15:26:52 +0100

    changed css folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./App.scss";
+import "./style/App.scss";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
