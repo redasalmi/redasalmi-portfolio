749614889c4ce6a20996070e5cac2d2c5e2100f7
redasalmi <reda.salmi.elt@gmail.com>
2020-09-20 13:15:46 +0100

    moved the skills info to a js file

---
+++ src/assets/data/skill.js
@@ -0,0 +1,101 @@
+import {
+  html,
+  css,
+  javascript,
+  python,
+  sass,
+  bootstrap,
+  react,
+  redux,
+  flask,
+  flaskWhite,
+  express,
+  expressWhite,
+  git,
+  npm,
+} from "../svg";
+
+export const SKILLS = {
+  programming: [
+    {
+      imgSrc: html,
+      imgAlt: "html",
+      skill: "HTML5",
+      alignTop: false,
+    },
+    {
+      imgSrc: css,
+      imgAlt: "css",
+      skill: "CSS3",
+      alignTop: false,
+    },
+    {
+      imgSrc: javascript,
+      imgAlt: "javascript",
+      skill: "JavaScript",
+      alignTop: false,
+    },
+    {
+      imgSrc: python,
+      imgAlt: "python",
+      skill: "Python",
+      alignTop: true,
+    },
+  ],
+
+  librairies: [
+    {
+      imgSrc: sass,
+      imgAlt: "sass",
+      skill: "SASS",
+      alignTop: true,
+    },
+    {
+      imgSrc: bootstrap,
+      imgAlt: "bootstrap",
+      skill: "Bootstrap",
+      alignTop: true,
+    },
+    {
+      imgSrc: react,
+      imgAlt: "react",
+      skill: "React",
+      alignTop: false,
+    },
+    {
+      imgSrc: redux,
+      imgAlt: "redux",
+      skill: "Redux",
+      alignTop: true,
+    },
+    {
+      imgSrc: flask,
+      imgAlt: "flask",
+      skill: "Flask",
+      alignTop: true,
+      whiteImgSrc: flaskWhite,
+    },
+    {
+      imgSrc: express,
+      imgAlt: "express",
+      skill: "ExpressJS",
+      alignTop: true,
+      whiteImgSrc: expressWhite,
+    },
+  ],
+
+  tools: [
+    {
+      imgSrc: git,
+      imgAlt: "git",
+      skill: "Git",
+      alignTop: false,
+    },
+    {
+      imgSrc: npm,
+      imgAlt: "npm",
+      skill: "NPM",
+      alignTop: false,
+    },
+  ],
+};
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,26 +1,29 @@
 import React, { useContext } from "react";
 import reactCertificate from "../assets/img/react-certificate.webp";
 import ImageLoader from "./ImageLoader";
-import {
-  html,
-  css,
-  javascript,
-  python,
-  sass,
-  bootstrap,
-  react,
-  redux,
-  flask,
-  express,
-  expressWhite,
-  git,
-  npm,
-  flaskWhite,
-} from "../assets/svg";
 import { ThemeDispatch } from "../reducer";
+import { SKILLS } from "../assets/data/skill";
+
+const Skill = ({ imgSrc, imgAlt, skill, alignTop, whiteImgSrc, theme }) => (
+  <li
+    className={`list-inline-item text-center ml-4 ${
+      alignTop ? "align-top" : ""
+    }`}
+  >
+    <img
+      src={whiteImgSrc && theme === "dark" ? whiteImgSrc : imgSrc}
+      width="50"
+      height="65"
+      alt={imgAlt}
+    />
+    <p>{skill}</p>
+  </li>
+);
 
 const Skills = ({ translate }) => {
   const { theme } = useContext(ThemeDispatch);
+  const { programming, librairies, tools } = SKILLS;
+
   return (
     <div className="row mt-md-5">
       <h1 className="col-12 text-primary">{translate("skills title")}</h1>
@@ -28,72 +31,55 @@ const Skills = ({ translate }) => {
       <div className="col-12 col-lg-6">
         <p className="col-12 lead mt-2">{translate("programming skills")}</p>
         <ul className="list-inline text-center text-lg-left">
-          <li className="list-inline-item text-center ml-4">
-            <img src={html} width="50" height="65" alt="html5" />
-            <p>HTML5</p>
-          </li>
-          <li className="list-inline-item text-center ml-4">
-            <img src={css} width="50" height="65" alt="css" />
-            <p>CSS3</p>
-          </li>
-          <li className="list-inline-item text-center ml-4">
-            <img src={javascript} width="50" height="65" alt="javascript" />
-            <p>Javascript</p>
-          </li>
-          <li className="list-inline-item text-center ml-4 align-top">
-            <img src={python} width="50" height="65" alt="python" />
-            <p>Python</p>
-          </li>
+          {programming.map((prog) => {
+            const { imgSrc, imgAlt, skill, alignTop } = prog;
+
+            return (
+              <Skill
+                key={skill}
+                imgSrc={imgSrc}
+                imgAlt={imgAlt}
+                skill={skill}
+                alignTop={alignTop}
+              />
+            );
+          })}
         </ul>
 
         <p className="col-12 lead mt-2">{translate("libraries title")}</p>
         <ul className="list-inline text-center text-lg-left">
-          <li className="list-inline-item text-center ml-4 align-top">
-            <img src={sass} width="50" height="65" alt="sass" />
-            <p>SASS</p>
-          </li>
-          <li className="list-inline-item text-center ml-4 align-top">
-            <img src={bootstrap} width="50" height="65" alt="bootstrap" />
-            <p>Bootstrap</p>
-          </li>
-          <li className="list-inline-item text-center ml-4">
-            <img src={react} width="50" height="65" alt="react" />
-            <p>React</p>
-          </li>
-          <li className="list-inline-item text-center ml-4 align-top">
-            <img src={redux} width="50" height="65" alt="redux" />
-            <p>Redux</p>
-          </li>
-          <li className="list-inline-item text-center ml-4 align-top">
-            <img
-              src={theme.theme === "dark" ? flaskWhite : flask}
-              width="50"
-              height="65"
-              alt="flask"
-            />
-            <p>Flask</p>
-          </li>
-          <li className="list-inline-item text-center ml-4 align-top">
-            <img
-              src={theme.theme === "dark" ? expressWhite : express}
-              width="50"
-              height="65"
-              alt="exrpessjs"
-            />
-            <p>ExpressJs</p>
-          </li>
+          {librairies.map((library) => {
+            const { imgSrc, imgAlt, skill, alignTop, whiteImgSrc } = library;
+
+            return (
+              <Skill
+                key={skill}
+                imgSrc={imgSrc}
+                imgAlt={imgAlt}
+                skill={skill}
+                alignTop={alignTop}
+                whiteImgSrc={whiteImgSrc}
+                theme={theme.theme}
+              />
+            );
+          })}
         </ul>
 
         <p className="col-12 lead mt-2">{translate("Tools & other")}</p>
         <ul className="list-inline text-center text-lg-left">
-          <li className="list-inline-item text-center ml-4">
-            <img src={git} width="50" height="65" alt="git" />
-            <p>Git</p>
-          </li>
-          <li className="list-inline-item text-center ml-4">
-            <img src={npm} width="50" height="65" alt="npm" />
-            <p>NPM</p>
-          </li>
+          {tools.map((tool) => {
+            const { imgSrc, imgAlt, skill, alignTop } = tool;
+
+            return (
+              <Skill
+                key={skill}
+                imgSrc={imgSrc}
+                imgAlt={imgAlt}
+                skill={skill}
+                alignTop={alignTop}
+              />
+            );
+          })}
         </ul>
       </div>
 
